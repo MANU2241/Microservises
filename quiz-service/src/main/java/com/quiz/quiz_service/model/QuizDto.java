3819b69866de495ce7aead347a0787b7d4c7e1aa
manu <manu@gmail.com>
2025-11-04 19:51:25 +0530

    initiaql commit

---
+++ quiz-service/src/main/java/com/quiz/quiz_service/model/QuizDto.java
@@ -0,0 +1,37 @@
+package com.quiz.quiz_service.model;
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+    String categoryName;
+    Integer numQuestions;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getNumQuestions() {
+        return numQuestions;
+    }
+
+    public void setNumQuestions(Integer numQuestions) {
+        this.numQuestions = numQuestions;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    String title;
+
+
+}
